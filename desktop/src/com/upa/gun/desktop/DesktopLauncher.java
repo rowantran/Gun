917f8aec2c63538f1b24044bae1237deeab219d2
Rowan-James Tran <rowanjames.tran@gmail.com>
2018-09-08 09:08:37 -0700

    Initial commit w/ Gradle

---
+++ desktop/src/com/upa/gun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.upa.gun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.upa.gun.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
