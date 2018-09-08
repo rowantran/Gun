a889dc3d6f19463a663e023398760fbbcbde33fa
Rowan-James Tran <rowanjames.tran@gmail.com>
2018-09-08 10:34:53 -0700

    Change window title

--- desktop/src/com/upa/gun/desktop/DesktopLauncher.java
+++ desktop/src/com/upa/gun/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.upa.gun.MainGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Gun";
+		config.width = 1280;
+		config.height = 800;
+
 		new LwjglApplication(new MainGame(), config);
 	}
 }
