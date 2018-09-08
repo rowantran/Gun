6b080a94a095c0f97e43be47de26b4ff21ac0d69
Rowan-James Tran <rowanjames.tran@gmail.com>
2018-09-08 10:20:21 -0700

    Change color

--- core/src/com/upa/gun/MainGame.java
+++ core/src/com/upa/gun/MainGame.java
@@ -18,7 +18,7 @@ public class MainGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.2f, 0, 0.8f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
