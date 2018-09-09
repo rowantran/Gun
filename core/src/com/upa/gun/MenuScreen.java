eeaef7df37c0caac587990c9409f83af8419f0d1
Rowan-James Tran <rowanjames.tran@gmail.com>
2018-09-09 01:18:50 -0700

    Remove % spawn, menu screen flashing

--- core/src/com/upa/gun/MenuScreen.java
+++ core/src/com/upa/gun/MenuScreen.java
@@ -3,6 +3,7 @@ package com.upa.gun;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 
@@ -11,6 +12,9 @@ public class MenuScreen extends ScreenAdapter {
     OrthographicCamera camera;
     GlyphLayout layout;
 
+    float textAlpha;
+    boolean fading;
+
     public MenuScreen(GunGame game) {
         this.game = game;
         camera = new OrthographicCamera();
@@ -18,7 +22,8 @@ public class MenuScreen extends ScreenAdapter {
 
         layout = new GlyphLayout();
 
-        Assets.menuFont.getData().setScale(8);
+        textAlpha = 1.0f;
+        fading = true;
     }
 
     public void draw() {
@@ -26,22 +31,48 @@ public class MenuScreen extends ScreenAdapter {
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.enableBlending();
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         game.batch.begin();
+        Assets.menuFont.setColor(1,1,1,1);
+        Assets.menuFont.getData().setScale(8);
         layout.setText(Assets.menuFont, "Gun");
         Assets.menuFont.draw(game.batch, layout, (Settings.RESOLUTION.x - layout.width) / 2,
-                (Settings.RESOLUTION.y/2 + layout.height*0.5f));
+                (Settings.RESOLUTION.y*4/5 + layout.height*0.5f));
+        game.batch.end();
+
+        game.batch.begin();
+        Assets.menuFont.setColor(1,1,1,textAlpha);
+        Assets.menuFont.getData().setScale(4);
+        layout.setText(Assets.menuFont, "Press any key to start");
+        Assets.menuFont.draw(game.batch, layout, (Settings.RESOLUTION.x - layout.width) / 2,
+                (Settings.RESOLUTION.x/6));
+
         game.batch.end();
     }
 
-    private void update() {
+    private void update(float delta) {
         if (Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
             game.setScreen(new GameScreen(game));
+        }
+
+        if (fading) {
+            textAlpha -= Settings.DEATH_FADE_SPEED * delta;
+            if (textAlpha <= 0.0f) {
+                fading = false;
+            }
+        } else {
+            textAlpha += Settings.DEATH_FADE_SPEED * delta;
+            if (textAlpha >= 1.0f) {
+                fading = true;
+            }
         }
     }
 
     @Override
     public void render(float delta) {
-        update();
+        update(delta);
         draw();
     }
 }
