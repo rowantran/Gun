804f8da9955fa0678ef6e6dd5ad239c720f728c9
Tushar Lakshminarayanan <tusharlaksh@gmail.com>
2018-09-09 03:38:33 -0700

    Added Boss Slime Spread Shot

--- core/src/com/upa/gun/BossSlime.java
+++ core/src/com/upa/gun/BossSlime.java
@@ -1,11 +1,28 @@
 package com.upa.gun;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class BossSlime extends Slime {
 
+	double interval;
+	
     public BossSlime(float x, float y, World world, GunWorld gunWorld) {
         super(x, y, world, gunWorld);
         shotInterval = 0.075f;
+        
+        interval = Math.PI/16;
+    }
+    
+    public void shoot() {
+        if (shooting) {
+            Vector2 slimePos = body.getTransform().getPosition();
+            for (int i = 0; i<32; i++) {
+            	double angle = interval * (double) i;
+            	gunWorld.bullets.add(new EnemyBullet(slimePos.x, slimePos.y, angle,
+                        world));
+            }
+            
+        }
     }
 }
