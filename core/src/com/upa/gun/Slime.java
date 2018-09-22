8b12034865f221513fe4aa5a8c7566ca3d96f777
Scott Lin <scottj.lin@gmail.com>
2018-09-21 21:10:50 -0700

    Cleaned up code for default movement. Added random movements for slimes (Move straight in a random direction for a random amount of time after a random interval).

--- core/src/com/upa/gun/Slime.java
+++ core/src/com/upa/gun/Slime.java
@@ -7,6 +7,12 @@ public class Slime extends Enemy {
     public static int RIGHT = 1;
 
     public float attackTimeElapsed;
+    public float timeSinceRandomMove; //time since last random move has been done
+    public float timeUntilRandomMove; //time between random moves, randomly generated
+    public float randomMoveLength; //length of time slime will travel in a random direction
+    public float timeOfRandomMove; //length of time slime has been travelling in a random direction
+
+    public boolean movingRandom = false;
 
     public float opacity;
 
@@ -27,6 +33,11 @@ public class Slime extends Enemy {
         super(gunWorld);
         attackTimeElapsed = 0.0f;
         timeSinceAttack = 0.0f;
+
+        timeSinceRandomMove = 0.0f;
+        timeUntilRandomMove = 0.0f;
+        randomMoveLength = 0.0f;
+        timeOfRandomMove = 0.0f;
 
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
@@ -58,6 +69,11 @@ public class Slime extends Enemy {
         super(gunWorld);
         attackTimeElapsed = 0.0f;
         timeSinceAttack = 0.0f;
+
+        timeSinceRandomMove = 0f;
+        timeUntilRandomMove = 0f;
+        randomMoveLength = 0.0f;
+        timeOfRandomMove = 0.0f;
 
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
@@ -117,46 +133,37 @@ public class Slime extends Enemy {
         }
     }
 
-    public void move(float delta) {
+
+    //default move toward player; horizontal or diagonal depending on position
+    public void defaultMove(float delta) {
         float playerX = gunWorld.player.body.getTransform().getPosition().x;
         float playerY = gunWorld.player.body.getTransform().getPosition().y;
 
         float slimeX = body.getTransform().getPosition().x;
         float slimeY = body.getTransform().getPosition().y;
 
-        if (slimeX < playerX) {
-            if (slimeY < playerY) {
-                body.setLinearVelocity(Settings.SLIME_SPEED * speedMultiplier, Settings.SLIME_SPEED * speedMultiplier);
-            }
-            if (slimeY > playerY) {
-                body.setLinearVelocity(Settings.SLIME_SPEED * speedMultiplier, -Settings.SLIME_SPEED * speedMultiplier);
-            }
-            if (slimeY == playerY) {
-                body.setLinearVelocity(Settings.SLIME_SPEED, 0);
-            }
+        float pythagMultiplier = 0.7071f;
+
+        if(slimeX < playerX) {
+            body.setLinearVelocity(Settings.SLIME_SPEED, 0);
+        } else if(slimeX > playerX) {
+            body.setLinearVelocity(-Settings.SLIME_SPEED, 0);
+        } else {
+            body.setLinearVelocity(0, 0);
         }
-        if (slimeX > playerX) {
-            if (slimeY < playerY) {
-                body.setLinearVelocity(-Settings.SLIME_SPEED * speedMultiplier, Settings.SLIME_SPEED * speedMultiplier);
-            }
-            if (slimeY > playerY) {
-                body.setLinearVelocity(-Settings.SLIME_SPEED * speedMultiplier, -Settings.SLIME_SPEED * speedMultiplier);
-            }
-            if (slimeY == playerY) {
-                body.setLinearVelocity(-Settings.SLIME_SPEED, 0);
-            }
+        if(slimeY < playerY) {
+            body.setLinearVelocity(body.getLinearVelocity().x * pythagMultiplier,
+                    Settings.SLIME_SPEED * pythagMultiplier);
+        } else if(slimeY > playerY) {
+            body.setLinearVelocity(body.getLinearVelocity().x * pythagMultiplier,
+                    -Settings.SLIME_SPEED * pythagMultiplier);
+        } else {
+            body.setLinearVelocity(body.getLinearVelocity().x, 0);
         }
-        if (slimeX == playerX) {
-            if (slimeY < playerY) {
-                body.setLinearVelocity(0, Settings.SLIME_SPEED);
-            }
-            if (slimeY > playerY) {
-                body.setLinearVelocity(0, -Settings.SLIME_SPEED);
-            }
-            if(slimeY == playerY) {
-                body.setLinearVelocity(0, 0);
-            }
+        if(body.getLinearVelocity().x == 0 && body.getLinearVelocity().y != 0) {
+            body.setLinearVelocity(0, body.getLinearVelocity().y / pythagMultiplier);
         }
+
 
         if(slimeX <= 113f/Settings.PPM && body.getLinearVelocity().x < 0) {
             body.setLinearVelocity(0, body.getLinearVelocity().y);
@@ -170,6 +177,68 @@ public class Slime extends Enemy {
         if(slimeY >= 674f/Settings.PPM && body.getLinearVelocity().y > 0) {
             body.setLinearVelocity(body.getLinearVelocity().x, 0);
         }
+    }
+
+
+    //check if the slime is on the edge of boundaries
+    public boolean checkBounds() {
+        float x = body.getTransform().getPosition().x;
+        float y = body.getTransform().getPosition().y;
+        return(x <= 113f/Settings.PPM || x >= 1160f/Settings.PPM || y <= 136f/Settings.PPM || y >= 674/Settings.PPM);
+    }
+
+
+    //move in a random direction, occurs on a random interval
+    public void randomMove() {
+        int x = (int)(Math.random() * Settings.SLIME_SPEED);
+        int y = getPythagY(x);
+        int switchX = (int)(Math.random() * 2); //indicates whether x and y direction should be positive or negative
+        int switchY = (int)(Math.random() * 2);
+
+        if(switchX == 1) {
+            x = -x;
+        }
+        if(switchY == 1) {
+            y = -y;
+        }
+        body.setLinearVelocity(x, y);
+    }
+
+    //uses pythagorean theorem to find corresponding y value for an x value to maintain speed
+    public int getPythagY(int x) {
+        return (int)(Math.sqrt(Math.pow(Settings.SLIME_SPEED,2) - Math.pow(x,2)));
+    }
+
+    //resets timers for random move and generates a new time until random move
+    public void resetRandomMove() {
+        int newRand1 = (int)(Math.random() * 2) + 1;
+        timeUntilRandomMove = (float)(newRand1);
+        timeSinceRandomMove = 0.0f;
+        randomMoveLength = (float)(Math.random() * 2) + 1;
+        timeOfRandomMove = 0.0f;
+        movingRandom = false;
+    }
+
+    public void move(float delta) {
+        if(timeSinceRandomMove < timeUntilRandomMove) { //normal moving time
+            defaultMove(delta);
+            timeSinceRandomMove += delta;
+        } else if(timeSinceRandomMove >= timeUntilRandomMove) { //random moving time
+            if(!movingRandom) { //just reached time
+                randomMove();
+                movingRandom = true;
+            } else { //random move already set
+                if(checkBounds()) { //slime is on a path to go out of bounds
+                    resetRandomMove();
+                }
+                if(timeOfRandomMove >= randomMoveLength) { //time limit reached
+                    resetRandomMove();
+                } else {
+                    timeOfRandomMove += delta; //time limit not reached
+                }
+            }
+        }
+
     }
 
     public void fireSound() {
